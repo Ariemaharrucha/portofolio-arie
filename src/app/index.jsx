b8acd76bdbeca74204094cecbe4fd4d154e14802
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-10-18 22:51:51 +0700

    add footer

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,6 +3,7 @@ import { ArticleSection } from "../components/section/article.section.jsx";
 import { ContactSection } from "../components/section/contact.section.jsx";
 import { HeroSection } from "../components/section/hero.section.jsx";
 import { PortofolioSection } from "../components/section/porto.section.jsx";
+import { Footer } from "../components/shared/footer.jsx";
 import { Header } from "../components/shared/header.jsx";
 
 export const App = () => {
@@ -15,6 +16,7 @@ export const App = () => {
         <PortofolioSection />
         <ContactSection />
         <ArticleSection />
+        <Footer />
       </section>
     </main>
   );
---
+++ src/components/shared/footer.jsx
@@ -0,0 +1,38 @@
+import { FaGithub } from "react-icons/fa";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-red-500 rounded-lg shadow m-4 ">
+      <div className="w-full mx-auto max-w-screen-xl px-4 py-3.5 font-medium flex items-center justify-between">
+        <span className="text-base text-white  ">
+          © 2024{" "}
+          <a href="https://github.com/Ariemaharrucha" target="_blank" className="hover:underline">
+            Arie maharrucha zakka
+          </a>.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-base font-medium text-white">
+          <li>
+            <a href="" className="hover:underline me-6 flex gap-2 items-center">
+              <FaGithub size={30}/> Github
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-6">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-6">
+              Licensing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
