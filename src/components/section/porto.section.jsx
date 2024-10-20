9108cbd21156f682f9eb898b79abe7299aca8609
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-10-20 15:17:13 +0700

    update card

--- src/components/section/porto.section.jsx
+++ src/components/section/porto.section.jsx
@@ -14,9 +14,9 @@ export const PortofolioSection = () => {
       <div className='mb-8'>
         <h3 className='text-center text-red-500 font-semibold'>Portofolio</h3>
       </div>
-      <div className='flex gap-4 justify-center flex-wrap '>
-        <Card url="https://i.pinimg.com/564x/3d/cc/48/3dcc48424393c6c5056f62ac29a9fe8b.jpg" stack={project1}></Card>
-        <Card url="https://i.pinimg.com/564x/3d/cc/48/3dcc48424393c6c5056f62ac29a9fe8b.jpg"></Card>
+      <div className='flex gap-6  justify-center flex-wrap '>
+        <Card url="https://i.pinimg.com/564x/3d/cc/48/3dcc48424393c6c5056f62ac29a9fe8b.jpg" stack={project1} desc={'lorem adasdasdad'}></Card>
+        <Card url="https://i.pinimg.com/564x/3d/cc/48/3dcc48424393c6c5056f62ac29a9fe8b.jpg" desc={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae corporis, praesentium porro amet explicabo ipsa rerum in rem repellendus esse voluptatibus incidunt nisi provident eos sapiente placeat. Deserunt, totam aliquid.'}></Card>
         <Card url="https://i.pinimg.com/564x/3d/cc/48/3dcc48424393c6c5056f62ac29a9fe8b.jpg"></Card>
       </div>
     </section>
--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,21 +1,22 @@
 export const Card = (props) => {
-  const { url, stack = [] } = props;
+  const { url, stack = [], desc } = props;
   return (
-    <div className="max-w-sm shadow-xl rounded-md  overflow-hidden">
+    <div className="w-96 flex flex-col shadow-xl rounded-md   overflow-hidden">
       <div className="h-[200px]">
         <img src={url} alt="" className="h-full w-full object-cover" />
       </div>
-      <div className="p-4 space-y-4 ">
+      <div className="flex-1 flex-col p-4 space-y-4">
         <h3>Title</h3>
-        <p className="h-full">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit maiores,
-          dolore similique laboriosam soluta ea nostrum culpa ex pariatur ab.
+        <p>
+          {desc}
         </p>
         <div className="flex gap-4 flex-wrap">
           {stack.map((item, index) => (
             <item.icon className={item.style} size={40} key={index} />
           ))}
         </div>
+      </div>
+      <div className="justify-end ">
         <button className="px-5 py-3 bg-red-500 block w-full text-white font-semibold rounded-md border-none hover:bg-red-600 transition-all">
           Projects
         </button>
